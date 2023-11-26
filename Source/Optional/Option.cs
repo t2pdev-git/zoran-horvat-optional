bbc2839fab7f99cb8e4caefba4639556fff3c0ed
Daryl Olson <Daryl Olson>
2023-11-26 12:42:47 -0700

    Correct SonarLint warnings

--- Source/Optional/Option.cs
+++ Source/Optional/Option.cs
@@ -1,37 +1,36 @@
 namespace CodingHelmet.Optional;
 
-public struct Option<T> : IEquatable<Option<T>> where T : class
+public readonly struct Option<T> : IEquatable<Option<T>> where T : class
 {
-    private T? _content;
-
-    public static Option<T> Some(T obj) => new() { _content = obj };
+    private T? Content { get; init; }
+    
+    public static Option<T> Some(T obj) => new() { Content = obj };
     public static Option<T> None() => new();
 
     public Option<TResult> Map<TResult>(Func<T, TResult> map) where TResult : class =>
-        new() { _content = _content is not null ? map(_content) : null };
+        new() { Content = Content is not null ? map(Content) : null };
     public ValueOption<TResult> MapValue<TResult>(Func<T, TResult> map) where TResult : struct =>
-        _content is not null ? ValueOption<TResult>.Some(map(_content)) : ValueOption<TResult>.None();
+        Content is not null ? ValueOption<TResult>.Some(map(Content)) : ValueOption<TResult>.None();
 
     public Option<TResult> MapOptional<TResult>(Func<T, Option<TResult>> map) where TResult : class =>
-        _content is not null ? map(_content) : Option<TResult>.None();
+        Content is not null ? map(Content) : Option<TResult>.None();
     public ValueOption<TResult> MapOptionalValue<TResult>(Func<T, ValueOption<TResult>> map) where TResult : struct =>
-        _content is not null ? map(_content) : ValueOption<TResult>.None();
+        Content is not null ? map(Content) : ValueOption<TResult>.None();
 
-    public T Reduce(T orElse) => _content ?? orElse;
-    public T Reduce(Func<T> orElse) => _content ?? orElse();
+    public T Reduce(T orElse) => Content ?? orElse;
+    public T Reduce(Func<T> orElse) => Content ?? orElse();
 
     public Option<T> Where(Func<T, bool> predicate) =>
-        _content is not null && predicate(_content) ? this : Option<T>.None();
+        Content is not null && predicate(Content) ? this : Option<T>.None();
 
     public Option<T> WhereNot(Func<T, bool> predicate) =>
-        _content is not null && !predicate(_content) ? this : Option<T>.None();
+        Content is not null && !predicate(Content) ? this : Option<T>.None();
 
-    public override int GetHashCode() => _content?.GetHashCode() ?? 0;
-    public override bool Equals(object? other) => other is Option<T> option && Equals(option);
+    public override int GetHashCode() => Content?.GetHashCode() ?? 0;
+    public override bool Equals(object? obj) => obj is Option<T> option && Equals(option);
 
     public bool Equals(Option<T> other) =>
-        _content is null ? other._content is null
-        : _content.Equals(other._content);
+        Content?.Equals(other.Content) ?? other.Content is null;
 
     public static bool operator ==(Option<T>? a, Option<T>? b) => a is null ? b is null : a.Equals(b);
     public static bool operator !=(Option<T>? a, Option<T>? b) => !(a == b);
--- Source/Optional/ValueOption.cs
+++ Source/Optional/ValueOption.cs
@@ -1,37 +1,38 @@
 namespace CodingHelmet.Optional;
 
-public struct ValueOption<T> : IEquatable<ValueOption<T>> where T : struct
+public readonly struct ValueOption<T> : IEquatable<ValueOption<T>> where T : struct
 {
-    private T? _content;
+    private T? Content { get; init; }
 
-    public static ValueOption<T> Some(T obj) => new() { _content = obj };
+    public static ValueOption<T> Some(T obj) => new() { Content = obj };
     public static ValueOption<T> None() => new();
 
     public Option<TResult> Map<TResult>(Func<T, TResult> map) where TResult : class =>
-        _content.HasValue ? Option<TResult>.Some(map(_content.Value)) : Option<TResult>.None();
+        Content.HasValue ? Option<TResult>.Some(map(Content.Value)) : Option<TResult>.None();
     public ValueOption<TResult> MapValue<TResult>(Func<T, TResult> map) where TResult : struct =>
-        new() { _content = _content.HasValue ? map(_content.Value) : null };
+        new() { Content = Content.HasValue ? map(Content.Value) : null };
 
     public Option<TResult> MapOptional<TResult>(Func<T, Option<TResult>> map) where TResult : class =>
-        _content.HasValue ? map(_content.Value) : Option<TResult>.None();
+        Content.HasValue ? map(Content.Value) : Option<TResult>.None();
     public ValueOption<TResult> MapOptionalValue<TResult>(Func<T, ValueOption<TResult>> map) where TResult : struct =>
-        _content.HasValue ? map(_content.Value) : ValueOption<TResult>.None();
+        Content.HasValue ? map(Content.Value) : ValueOption<TResult>.None();
 
-    public T Reduce(T orElse) => _content ?? orElse;
-    public T Reduce(Func<T> orElse) => _content ?? orElse();
+    public T Reduce(T orElse) => Content ?? orElse;
+    public T Reduce(Func<T> orElse) => Content ?? orElse();
 
     public ValueOption<T> Where(Func<T, bool> predicate) =>
-        _content.HasValue && predicate(_content.Value) ? this : ValueOption<T>.None();
+        Content.HasValue && predicate(Content.Value) ? this : ValueOption<T>.None();
 
     public ValueOption<T> WhereNot(Func<T, bool> predicate) =>
-        _content.HasValue && !predicate(_content.Value) ? this : ValueOption<T>.None();
+        Content.HasValue && !predicate(Content.Value) ? this : ValueOption<T>.None();
 
-    public override int GetHashCode() => _content?.GetHashCode() ?? 0;
-    public override bool Equals(object? other) => other is ValueOption<T> option && Equals(option);
+    public override int GetHashCode() => Content?.GetHashCode() ?? 0;
+    public override bool Equals(object? obj) => obj is ValueOption<T> option && Equals(option);
 
     public bool Equals(ValueOption<T> other) =>
-        _content.HasValue ? other._content.HasValue && _content.Value.Equals(other._content.Value)
-        : !other._content.HasValue;
+        Content.HasValue 
+            ? other.Content.HasValue && Content.Value.Equals(other.Content.Value)
+            : !other.Content.HasValue;
 
     public static bool operator ==(ValueOption<T> a, ValueOption<T> b) => a.Equals(b);
     public static bool operator !=(ValueOption<T> a, ValueOption<T> b) => !(a.Equals(b));
